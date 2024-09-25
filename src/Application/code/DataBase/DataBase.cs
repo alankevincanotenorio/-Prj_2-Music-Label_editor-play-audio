08e4061a26e2d534cf36112abfb1139f3eb2671f
AlanKevinCT <Alan.Kevin@ciencias.unam.mx>
2024-09-25 16:22:45 -0600

    Organize the code folder

--- src/Application/code/DataBase/DataBase.cs
+++ src/Application/code/DataBase/DataBase.cs
@@ -10,7 +10,7 @@ public class DataBase
     //Constructor
     private DataBase()  
     {
-        string dbPath = "./src/Application/code/data/dataBase.db";
+        string dbPath = "./src/Application/code/DataBase/data/dataBase.db";
         bool dbExists = File.Exists(dbPath);
         string connectionString = $"Data Source={dbPath};Version=3;";
         _connection = new SQLiteConnection(connectionString);
@@ -222,31 +222,31 @@ public class DataBase
     {
         DataBase db = DataBase.Instance;
         Console.WriteLine("Data base ready to use");
-        //add rola example
-        Rola rola = new Rola(1, 1, 1, "/pathExample/ExampleSong.mp3", " Example1", 1, 2024, "example");
-        bool isInserted = db.InsertRola(rola);
-        if (isInserted)
-        {
-            rola.ShowInfo();
-        }
-        //add performer example
-        Performer p = new Performer(3, "M2222", 2);
-        bool isInsertedP = db.InsertPerformer(p);
-        if (isInsertedP)
-        {
-            p.ShowInfo();
-        }
-        //get performer example
-        Performer? retrievedPerformer = db.GetPerformerById(100);
-        if (retrievedPerformer != null)
-        {
-            Console.WriteLine("Performer founded: ");
-            retrievedPerformer.ShowInfo();
-        }
-        else
-        {
-            Console.WriteLine("Performer does not exists");
-        }
+        // //add rola example
+        // Rola rola = new Rola(1, 1, 1, "/pathExample/ExampleSong.mp3", " Example1", 1, 2024, "example");
+        // bool isInserted = db.InsertRola(rola);
+        // if (isInserted)
+        // {
+        //     rola.ShowInfo();
+        // }
+        // //add performer example
+        // Performer p = new Performer(3, "M2222", 2);
+        // bool isInsertedP = db.InsertPerformer(p);
+        // if (isInsertedP)
+        // {
+        //     p.ShowInfo();
+        // }
+        // //get performer example
+        // Performer? retrievedPerformer = db.GetPerformerById(100);
+        // if (retrievedPerformer != null)
+        // {
+        //     Console.WriteLine("Performer founded: ");
+        //     retrievedPerformer.ShowInfo();
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Performer does not exists");
+        // }
         string musicPath = "/home/alan/Downloads";
         Miner miner = new Miner(musicPath);
         if(miner.BrowsePaths(musicPath))
