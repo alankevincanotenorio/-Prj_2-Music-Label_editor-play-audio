53d7d80ed323994bee16406942e66bb8d87b6466
AlanKevinCT <Alan.Kevin@ciencias.unam.mx>
2024-09-29 18:09:41 -0600

    Begin of the UI: 	-I added a new project called GraphicInterface to implement the view of the MVC 	-Program.cs only has the basic implementation to create an empty window with a title.

---
+++ GraphicInterface/Program.cs
@@ -0,0 +1,19 @@
+﻿using Gtk;
+ 
+class SharpApp : Window {
+ 
+    public SharpApp() : base("MusicLabelEditor")
+    {
+        SetDefaultSize(250, 200);
+        SetPosition(WindowPosition.Center);
+        DeleteEvent += delegate { Application.Quit(); }; //x or alt+f4 to close
+        Show();
+    }
+    
+    public static void Main()
+    {
+        Application.Init();
+        new SharpApp();
+        Application.Run();
+    }
+}
