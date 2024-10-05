ed28cb2393656ae4e47ebf3ba5554a25d8bca659
AlanKevinCT <Alan.Kevin@ciencias.unam.mx>
2024-10-04 22:28:56 -0600

    The UI now has: 	- Start Mining button working. 	- Background color has been changed to grey. 	- The Table has been changed to Grid, removing compilation warnings. 	- Change Path button working.

--- GraphicInterface/GraphicInterface.cs
+++ GraphicInterface/GraphicInterface.cs
@@ -1,62 +1,142 @@
 ﻿using Gtk;
 using ControllerApp;
+using System.Collections.Generic;
 
 class GraphicInterface : Window
 {
     public Controller app = new Controller();  
-    private VBox vbox;
+    private TextView rolasList;
+    private Label currentPathLabel;
+    CssProvider cssProvider = new CssProvider();
 
     public GraphicInterface() : base("Music Library Editor")
     {
-        SetDefaultSize(400, 300);
+        SetDefaultSize(800, 600);
         SetPosition(WindowPosition.Center);
-        
+        BorderWidth = 10;
 
-        DeleteEvent += delegate { Application.Quit(); };
+        //to change background color
+        cssProvider.LoadFromData(@"
+            textview {
+                background-color: #a9a9a9; /* Fondo gris claro para el TextView */
+                color: #000000; /* Color del texto */
+            }
+            text {
+                background-color: #a9a9a9; /* Fondo gris claro para el área de texto */
+                color: #282828; /* Color del texto */
+            }
+            window {
+                background-color: #999999; /* Fondo gris para la ventana */
+            }
+        ");
+        StyleContext.AddProviderForScreen(Gdk.Screen.Default, cssProvider, 800);
 
-        Fixed fix = new Fixed();
-        Button miningButton = new Button("Start Mining");
-        miningButton.Clicked += OnMineClick;
-        miningButton.SetSizeRequest(100, 40);
-        fix.Put(miningButton, 500, 60);
+        //close app
+        DeleteEvent += (sender, args) => Application.Quit();
 
-        Button changeDirButton = new Button("Change");
+        Box mainBox = new Box(Orientation.Vertical, 10);
+
+        Grid grid = new Grid();
+        grid.RowSpacing = 10;
+        grid.ColumnSpacing = 10;
+        grid.Margin = 10;
+
+        // show current path
+        currentPathLabel = new Label($"Current Path: {app.GetCurrentPath()}");
+        Box pathBox = new Box(Orientation.Horizontal, 10);
+        pathBox.PackStart(currentPathLabel, true, true, 5);
+
+        Button changeDirButton = new Button("Change Path");
         changeDirButton.SetSizeRequest(100, 40);
-        fix.Put(changeDirButton, 500, 20);
+        changeDirButton.Clicked += OnChangeDirClick!;
+        pathBox.PackStart(changeDirButton, false, false, 5);
+        grid.Attach(pathBox, 0, 0, 4, 1);
 
+        // TextView to show mined rolas
+        rolasList = new TextView();
+        rolasList.StyleContext.AddProvider(cssProvider, uint.MaxValue);
+        rolasList.Buffer.Text = "Rolas will be displayed here...";
+        rolasList.WrapMode = WrapMode.Word;
+        rolasList.Editable = false;
+        rolasList.Vexpand = true;
+        rolasList.Hexpand = true;
+        grid.Attach(rolasList, 0, 1, 3, 4);
+
+        Box buttonBox = new Box(Orientation.Vertical, 10);
+
+        // "Start mining"
+        Button miningButton = new Button("Start Mining");
+        miningButton.SetSizeRequest(100, 40);
+        miningButton.Clicked += OnMineClick!;
+        buttonBox.PackStart(miningButton, false, false, 0);
+
+        // "Edit"
         Button editButton = new Button("Edit");
         editButton.SetSizeRequest(100, 40);
-        fix.Put(editButton, 500, 100);
+        buttonBox.PackStart(editButton, false, false, 0);
 
-        Button searchButton = new Button("Search"); //insert lupa icon
-        searchButton.SetSizeRequest(100, 40);
-        fix.Put(searchButton, 500, 160);
+        // "Help"
+        Button helpButton = new Button("Help");
+        helpButton.SetSizeRequest(100, 40);
+        buttonBox.PackStart(helpButton, false, false, 0);
 
-        Button burgerButton = new Button("Burger"); //insert burger icon
-        burgerButton.SetSizeRequest(100, 40);        
-        fix.Put(burgerButton, 500, 200);
-    
-        vbox = new VBox();
-        fix.Put(vbox, 50, 100);  
-        Add(fix);
+        // "Search"
+        Button searchButton = new Button();
+        Image searchIcon = new Image(Stock.Find, IconSize.Button);
+        searchButton.Image = searchIcon;
+        searchButton.SetSizeRequest(40, 40);
+        buttonBox.PackStart(searchButton, false, false, 0);
+
+        // "Burger"
+        Button burgerButton = new Button();
+        Image burgerImage = new Image(Stock.Index, IconSize.Button);
+        burgerButton.Image = burgerImage;
+        burgerButton.SetSizeRequest(40, 40);
+        buttonBox.PackStart(burgerButton, false, false, 0);
+
+        grid.Attach(buttonBox, 3, 1, 1, 5);
+
+        // add grid
+        mainBox.PackStart(grid, true, true, 0);
+        Add(mainBox);
+
         ShowAll();
     }
 
+    // manage miner
     void OnMineClick(object sender, EventArgs args)
     {
-        foreach (Widget child in vbox.Children)
-        {
-            vbox.Remove(child); 
-            child.Destroy();    
-        }
-        app.StartMining(); 
+        app.StartMining();
         List<string> rolas = app.GetRolasInfoInPath();
-        foreach (string title in rolas)
-        {
-            Label titleLabel = new Label(title); 
-            vbox.PackStart(titleLabel, false, false, 5);
-        }
-        vbox.ShowAll();  
+        rolasList.Buffer.Text = string.Join("\n", rolas);
+    }
+
+    // manage change directory
+    void OnChangeDirClick(object sender, EventArgs args)
+    {
+        Window changePathWindow = new Window("Change Path");
+        changePathWindow.SetDefaultSize(300, 100);
+        changePathWindow.SetPosition(WindowPosition.Center);
+        changePathWindow.StyleContext.AddProvider(cssProvider, 800);
+
+        Box vbox = new Box(Orientation.Vertical, 10);
+        Label instructionLabel = new Label("Insert the new path:");
+        vbox.PackStart(instructionLabel, false, false, 5);
+
+        Entry pathEntry = new Entry();
+        vbox.PackStart(pathEntry, false, false, 5);
+
+        Button confirmButton = new Button("Confirm");
+        confirmButton.Clicked += (s, e) => {
+            string newPath = pathEntry.Text;
+            app.SetCurrentPath(newPath);
+            currentPathLabel.Text = $"Current Path: {newPath}";
+            changePathWindow.Destroy();
+        };
+        vbox.PackStart(confirmButton, false, false, 5);
+
+        changePathWindow.Add(vbox);
+        changePathWindow.ShowAll();
     }
 
     public static void Main()
